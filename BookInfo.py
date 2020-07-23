89268923f15bb3dc351dd424aa3954909b90cd86
Kinji_Kamizaki <Kinji.Kamizaki@gmail.com>
2020-07-24 00:09:19 +0900

    module

---
+++ BookInfo.py
@@ -0,0 +1,51 @@
+﻿import unicodedata
+
+# 半角、全角をそれぞれ1文字、2文字として文字数をカウントする
+def len_count(text):
+    t_count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            t_count += 2
+        else:
+            t_count += 1
+    return t_count
+
+# 本の情報用クラス作成
+class BookInfo:
+
+    # コンストラクタ
+    def __init__(self, title, auther, url):
+        self.title = title
+        self.auther = auther
+        self.url = url
+
+    # タイトルに改行を入れるメソッド
+    def LinedTitle(self):
+        lined_title=[]
+        count=0
+        for char in self.title:
+            count = count + len_count(char)
+            lined_title.append(char)
+            if count > 40:
+                lined_title.append("\n")
+                count = 0
+        # 最後が改行記号なら削除する
+        if lined_title[-1] == "\n":
+            del lined_title[-1]
+        
+        self.title = "".join(lined_title)
+    
+    # 著者名が長すぎる場合はカットするメソッド
+    def CutAuther(self):
+        cut_auther=[]
+        count=0
+        for char in self.auther:
+            count = count + len_count(char)
+            cut_auther.append(char)
+            if count > 40:
+                break
+        self.auther = "".join(cut_auther)
+
+    # urlを絶対パス表記に変換するメソッド
+    def UrlConvert(self):
+        self.url = "https://bookmeter.com" + self.url
--- LR_list.py
+++ LR_list.py
@@ -5,8 +5,8 @@ from openpyxl.styles.borders import Border, Side
 import requests
 import time
 import sys
-import unicodedata
 from bs4 import BeautifulSoup
+from BookInfo import BookInfo
 i=0
 
 MAX_NUM = 50 # 読んだ冊数の最大値（デフォルト=50）
@@ -18,59 +18,11 @@ Booknames=[] #書籍名用配列
 Bookurls=[] # 本のURL用配列
 
 
-# 半角、全角をそれぞれ1文字、2文字として文字数をカウントする
-def len_count(text):
-    t_count = 0
-    for c in text:
-        if unicodedata.east_asian_width(c) in 'FWA':
-            t_count += 2
-        else:
-            t_count += 1
-    return t_count
+
 
 # 数字をアルファベットに変換
 def num2alpha(number):
     return chr(number+64)
-
-# 本の情報用クラス作成
-class BookInfo:
-
-    # コンストラクタ
-    def __init__(self, title, auther, url):
-        self.title = title
-        self.auther = auther
-        self.url = url
-
-    # タイトルに改行を入れるメソッド
-    def LinedTitle(self):
-        lined_title=[]
-        count=0
-        for char in self.title:
-            count = count + len_count(char)
-            lined_title.append(char)
-            if count > 40:
-                lined_title.append("\n")
-                count = 0
-        # 最後が改行記号なら削除する
-        if lined_title[-1] == "\n":
-            del lined_title[-1]
-        
-        self.title = "".join(lined_title)
-    
-    # 著者名が長すぎる場合はカットするメソッド
-    def CutAuther(self):
-        cut_auther=[]
-        count=0
-        for char in self.auther:
-            count = count + len_count(char)
-            cut_auther.append(char)
-            if count > 40:
-                break
-        self.auther = "".join(cut_auther)
-
-    # urlを絶対パス表記に変換するメソッド
-    def UrlConvert(self):
-        self.url = "https://bookmeter.com" + self.url
 
 #csv読み込み
 with open("attend.csv") as f:
